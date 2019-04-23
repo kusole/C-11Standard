698f8aa918a97e4eef1ad1d4eb296e0ba14af58a
kusole <lt19942013@163.com>
2019-04-23 21:09:51 +0800

    添加静态成员static

---
+++ Inc/04-account.h
@@ -0,0 +1,13 @@
+#ifndef __04_ACCOUNT__
+#define __04_ACCOUNT__
+
+class Account
+{
+public:
+    static double mRate;
+    static void set_rate(const double &x) { mRate = x; }
+};
+//double Account::mRate;可以不给初值
+double Account::mRate = 8.0;
+
+#endif
