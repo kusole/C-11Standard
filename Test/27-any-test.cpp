39bb66288a721e6df9adcb9df3294c61e1e23989
kusole <lt19942013@163.com>
2019-05-16 21:38:06 +0800

    add any modify

--- Test/27-any-test.cpp
+++ Test/27-any-test.cpp
@@ -4,8 +4,11 @@ int main() {
     Any n;
     auto r = n.IsNull();
     std::string s1 = "hello";
+    std::string s2 = "world";
     n = s1;
-    n.AnyCase<int> ();
+    cout << n.AnyCase<string> () << endl;
+    n = s2;
+    cout << n.AnyCase<string> () << endl;
     Any n1 = 1;
     n1.Is<int> ();
     return 0;
